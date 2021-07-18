ed2983a2fa81a00b33ae70ced8dcaae3816eb3c5
Harold Torres Marino <haroldtm55@gmail.com>
2021-07-18 18:02:42 +1000

    App working db.json fixed

--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -1,10 +1,8 @@
-import { render } from '@testing-library/react';
 import React from 'react';
 import ToyCard from './ToyCard'
 
 
 const ToyContainer = (props) => {
-  console.log(props)
   return(
     <div id="toy-collection">
       {props.toys.map(toy => <ToyCard key={toy.id} toy={toy} removeToyCard={props.removeToyCard} updateToyLikes={props.updateToyLikes}/>)}
